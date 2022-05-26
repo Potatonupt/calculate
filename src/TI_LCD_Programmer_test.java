7056613e26c6be9a6cb6e123a1425142d734f45d
梅家诚 <1468856453@qq.com>
2022-05-26 22:59:07 +0800

    完成了图形化界面以及相关功能实现

--- src/TI_LCD_Programmer_test.java
+++ src/TI_LCD_Programmer_test.java
@@ -4,6 +4,8 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
 import java.awt.*;
 import java.awt.event.*;
+import java.math.BigDecimal;
+import java.security.Key;
 
 public class TI_LCD_Programmer_test extends JFrame
 {
@@ -77,7 +79,17 @@ public class TI_LCD_Programmer_test extends JFrame
     private JTextField MidSpacer;
     private JLabel DECLabel;
     private JLabel HEXLabel;
-
+    private int symbol = 1;
+    private boolean if_operator = false;   //是否是运算符
+    private String operator1 = "";            //操作数1
+    private String operator2 = "";            //操作数2
+    private String tmp = "";                  //用于在ioput中显示
+    private BigDecimal first=new BigDecimal(0);
+    private BigDecimal firstmul=new BigDecimal(1);
+    private BigDecimal second=new BigDecimal(0);
+    private BigDecimal answer=new BigDecimal(0);
+    private int lastoperator=0;               //上一运算符
+    private int nowoperator=0;                //本次运算符
 
     public TI_LCD_Programmer_test() {
         setToolButton();
@@ -85,6 +97,8 @@ public class TI_LCD_Programmer_test extends JFrame
         setLabel();
         setText();
         setPanel();
+        keyboard_init();
+        button_init();
     }
 
     public static void main(String[] args) {
@@ -183,7 +197,7 @@ public class TI_LCD_Programmer_test extends JFrame
         DECLabel.setFont(BaseFont);
         DECLabel.setBorder(BorderFactory.createEmptyBorder());
         HEXLabel.setFont(BaseFont);//设置字体样式
-        HEXLabel.setText("HEX");//设置默认的文本
+//        HEXLabel.setText("HEX");//设置默认的文本
         HEXLabel.setForeground(Color.BLACK);//设置字体颜色
         HEXLabel.setFont(BaseFont);
         HEXLabel.setBorder(BorderFactory.createEmptyBorder());
@@ -212,7 +226,7 @@ public class TI_LCD_Programmer_test extends JFrame
         EmptySpacer1.setBorder(BorderFactory.createEmptyBorder());
         EmptySpacer2.setBorder(BorderFactory.createEmptyBorder());
         MidSpacer.setBorder(BorderFactory.createEmptyBorder());
-        IOput.setText("12345678");
+//        IOput.setText("12345678");
         Separator.setForeground(new Color(0xFFFFFF));
     }
     public void setPanel()//对面板进行设置
@@ -243,4 +257,196 @@ public class TI_LCD_Programmer_test extends JFrame
                 super.insertString(offset, str, attr);
         }
     }
-}
+
+        private void keyboard_init()
+        {
+            JButton[] DIGIT={a0Button,a1Button,a2Button,a3Button,a4Button,a5Button,
+                    a6Button,a7Button,a8Button,a9Button,AddButton,SubButton,
+                    MulButton,DivButton,dotButton,EqualButton};//,AddButton,SubButton,MulButton,DivButton,dotButton,EqualButton,CEButton
+            String[] pressed={"pressed 0","pressed NUMPAD0","pressed 1","pressed NUMPAD1","pressed 2","pressed NUMPAD2",
+                    "pressed 3","pressed NUMPAD3","pressed 4","pressed NUMPAD4","pressed 5","pressed NUMPAD5",
+                    "pressed 6","pressed NUMPAD6","pressed 7","pressed NUMPAD7","pressed 8","pressed NUMPAD8",
+                    "pressed 9","pressed NUMPAD9","pressed ADD","pressed shitf EQUALS","pressed SUBTRACT","pressed MINUS",
+                    "pressed MULTIPLY","pressed shift 8","pressed DIVIDE","pressed /","pressed DECIMAL","pressed DOT","pressed ENTER"};
+            for(int i=0;i<pressed.length;i+=2)
+            {
+                int finalI = i;
+                DIGIT[finalI].registerKeyboardAction(e->DIGIT[finalI].doClick(),KeyStroke.getKeyStroke(pressed[i]),JComponent.WHEN_IN_FOCUSED_WINDOW);
+                int finalJ=i+1;
+                DIGIT[finalJ].registerKeyboardAction(e->DIGIT[finalJ].doClick(),KeyStroke.getKeyStroke(pressed[i]),JComponent.WHEN_IN_FOCUSED_WINDOW);
+            }
+//            a0Button.registerKeyboardAction(e -> a0Button.doClick(), KeyStroke.getKeyStroke("pressed 0"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a0Button.registerKeyboardAction(e -> a0Button.doClick(), KeyStroke.getKeyStroke("pressed NUMPAD0"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a1Button.registerKeyboardAction(e -> a1Button.doClick(), KeyStroke.getKeyStroke("pressed 1"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a1Button.registerKeyboardAction(e -> a1Button.doClick(), KeyStroke.getKeyStroke("pressed NUMPAD1"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a2Button.registerKeyboardAction(e -> a2Button.doClick(), KeyStroke.getKeyStroke("pressed 2"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a2Button.registerKeyboardAction(e -> a2Button.doClick(), KeyStroke.getKeyStroke("pressed NUMPAD2"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a3Button.registerKeyboardAction(e -> a3Button.doClick(), KeyStroke.getKeyStroke("pressed 3"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a3Button.registerKeyboardAction(e -> a3Button.doClick(), KeyStroke.getKeyStroke("pressed NUMPAD3"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a4Button.registerKeyboardAction(e -> a4Button.doClick(), KeyStroke.getKeyStroke("pressed 4"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a4Button.registerKeyboardAction(e -> a4Button.doClick(), KeyStroke.getKeyStroke("pressed NUMPAD4"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a5Button.registerKeyboardAction(e -> a5Button.doClick(), KeyStroke.getKeyStroke("pressed 5"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a5Button.registerKeyboardAction(e -> a5Button.doClick(), KeyStroke.getKeyStroke("pressed NUMPAD5"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a6Button.registerKeyboardAction(e -> a6Button.doClick(), KeyStroke.getKeyStroke("pressed 6"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a6Button.registerKeyboardAction(e -> a6Button.doClick(), KeyStroke.getKeyStroke("pressed NUMPAD6"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a7Button.registerKeyboardAction(e -> a7Button.doClick(), KeyStroke.getKeyStroke("pressed 7"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a7Button.registerKeyboardAction(e -> a7Button.doClick(), KeyStroke.getKeyStroke("pressed NUMPAD7"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a8Button.registerKeyboardAction(e -> a8Button.doClick(), KeyStroke.getKeyStroke("pressed 8"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a8Button.registerKeyboardAction(e -> a8Button.doClick(), KeyStroke.getKeyStroke("pressed NUMPAD8"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a9Button.registerKeyboardAction(e -> a9Button.doClick(), KeyStroke.getKeyStroke("pressed 9"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            a9Button.registerKeyboardAction(e -> a9Button.doClick(), KeyStroke.getKeyStroke("pressed NUMPAD9"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            AddButton.registerKeyboardAction(e -> AddButton.doClick(), KeyStroke.getKeyStroke("pressed ADD"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            AddButton.registerKeyboardAction(e -> AddButton.doClick(), KeyStroke.getKeyStroke("pressed EQUALS"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            SubButton.registerKeyboardAction(e -> SubButton.doClick(), KeyStroke.getKeyStroke("pressed SUBTRACT"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            SubButton.registerKeyboardAction(e -> SubButton.doClick(), KeyStroke.getKeyStroke("pressed MINUS"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            MulButton.registerKeyboardAction(e -> MulButton.doClick(), KeyStroke.getKeyStroke("pressed MULTIPLY"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            DivButton.registerKeyboardAction(e -> DivButton.doClick(), KeyStroke.getKeyStroke("pressed DIVIDE"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            dotButton.registerKeyboardAction(e -> dotButton.doClick(), KeyStroke.getKeyStroke("pressed DECIMAL"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            EqualButton.registerKeyboardAction(e -> EqualButton.doClick(), KeyStroke.getKeyStroke("pressed ENTER"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+//            CEButton.registerKeyboardAction(e -> CEButton.doClick(), KeyStroke.getKeyStroke("pressed DELETE"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+        }
+
+        private void button_init()
+        {
+            a0Button.addActionListener(e ->
+            {
+                if_operator = false;
+                IOput_display("0");
+            });
+            a1Button.addActionListener(e ->
+            {
+                if_operator = false;
+                IOput_display("1");
+            });
+            a2Button.addActionListener(e ->
+            {
+                if_operator = false;
+                IOput_display("2");
+            });
+            a3Button.addActionListener(e -> {
+                if_operator = false;
+                IOput_display("3");
+            });
+            a4Button.addActionListener(e -> {
+                if_operator = false;
+                IOput_display("4");
+            });
+            a5Button.addActionListener(e -> {
+                if_operator = false;
+                IOput_display("5");
+            });
+            a6Button.addActionListener(e -> {
+                if_operator = false;
+                IOput_display("6");
+            });
+            a7Button.addActionListener(e -> {
+                if_operator = false;
+                IOput_display("7");
+            });
+            a8Button.addActionListener(e -> {
+                if_operator = false;
+                IOput_display("8");
+            });
+            a9Button.addActionListener(e -> {
+                if_operator = false;
+                IOput_display("9");
+            });
+            dotButton.addActionListener(e -> {
+                if_operator = false;
+                IOput_display(".");
+            });
+            AddButton.addActionListener(e -> {
+                if_operator = true;
+                nowoperator = 1;
+                operator2 = "" + tmp;
+                if (!operator2.isEmpty())
+                    second = new BigDecimal(operator2);
+                else
+                    second = new BigDecimal(0);
+                calculate();
+                IOput_display(answer.toString());
+                tmp = "";
+                first = answer;
+                lastoperator = nowoperator;
+            });
+            SubButton.addActionListener(e -> {
+                if_operator = true;
+                nowoperator = 2;
+                operator2 = IOput.getText();
+                if (!operator2.isEmpty())
+                    second = new BigDecimal(operator2);
+                else
+                    second = new BigDecimal(0);
+                calculate();
+                IOput_display(answer.toString());
+                tmp = "";
+                first = answer;
+                lastoperator = nowoperator;
+            });
+            MulButton.addActionListener(e -> {
+                if_operator = true;
+                nowoperator = 3;
+                operator2 = IOput.getText();
+                if (!operator2.isEmpty())
+                    second = new BigDecimal(operator2);
+                else
+                    second = new BigDecimal(0);
+                calculate();
+                IOput_display(answer.toString());
+                tmp = "";
+                first = answer;
+                lastoperator = nowoperator;
+            });
+            DivButton.addActionListener(e -> {
+                if_operator = true;
+                nowoperator = 4;
+                operator2 = IOput.getText();
+                if (!operator2.isEmpty())
+                    second = new BigDecimal(operator2);
+                else
+                    second = new BigDecimal(0);
+                calculate();
+                IOput_display(answer.toString());
+                tmp = "";
+                first = answer;
+                lastoperator = nowoperator;
+            });
+            EqualButton.addActionListener(e -> {
+                if_operator = true;
+                nowoperator = 0;
+                operator2 = IOput.getText();
+                if (!operator2.isEmpty())
+                    second = new BigDecimal(operator2);
+                else
+                    second = new BigDecimal(0);
+                calculate();
+                IOput_display(answer.toString());
+                tmp = "";
+                first = answer;
+                lastoperator = nowoperator;
+            });
+            CEButton.addActionListener(e -> {
+            });
+        }
+
+        private void calculate()
+        {
+            switch (lastoperator)
+            {
+                case 1:answer=first.add(second);break;
+                case 2:answer=first.subtract(second);break;
+                case 3:answer=first.multiply(second);break;
+                case 4:answer=first.divide(second);break;
+                case 0:answer=second;
+            }
+        }
+
+        private void IOput_display(String s)
+        {
+            if(if_operator==true)
+                tmp="";
+            tmp = tmp + s;
+            IOput.setText(tmp);
+        }
+
+    }
